f8fca693426ca470bddf00342e7cec086c204acd
Artufelii <artufeii@gmail.com>
2023-04-10 12:45:24 -0600

    Fix linters errors on styles and javascript

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -1,25 +1,25 @@
-const header = document.getElementById('header') 
-const openMenuButton = document.getElementById('open-menu-button') 
-const closeMenuButton = document.getElementById('close-menu-button') 
-const mobileMenu = document.getElementById('mobile-menu') 
-const main = document.getElementById('main') 
-//const mobileMenuOptions = document.getElementById('mobile-menu-list').querySelectorAll('li')
+const header = document.getElementById('header');
+const openMenuButton = document.getElementById('open-menu-button');
+const closeMenuButton = document.getElementById('close-menu-button');
+const mobileMenu = document.getElementById('mobile-menu');
+const main = document.getElementById('main');
+// const mobileMenuOptions = document.getElementById('mobile-menu-list').querySelectorAll('li')
 
 const openMobileMenu = () => {
-	mobileMenu.classList.add('show')
-	header.classList.add('blur')
-	main.classList.add('blur')
-}
+  mobileMenu.classList.add('show');
+  header.classList.add('blur');
+  main.classList.add('blur');
+};
 
 const closeMobileMenu = () => {
-	mobileMenu.classList.remove('show')
-	header.classList.remove('blur')
-	main.classList.remove('blur')
-}
+  mobileMenu.classList.remove('show');
+  header.classList.remove('blur');
+  main.classList.remove('blur');
+};
 
-openMenuButton.addEventListener('click', openMobileMenu)
-closeMenuButton.addEventListener('click', closeMobileMenu)
+openMenuButton.addEventListener('click', openMobileMenu);
+closeMenuButton.addEventListener('click', closeMobileMenu);
 
-//mobileMenuOptions.forEach(opt => {
-	//opt.addEventListener('click', closeMobileMenu)
-//})
+// mobileMenuOptions.forEach(opt => {
+// opt.addEventListener('click', closeMobileMenu)
+// })
