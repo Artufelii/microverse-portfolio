da985d61774b0e9609dfb3d76182aa203d1618de
Artufelii <artufeii@gmail.com>
2023-04-10 14:15:20 -0600

    Add new features to header and functionality

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -1,25 +1,22 @@
-const header = document.getElementById('header');
 const openMenuButton = document.getElementById('open-menu-button');
 const closeMenuButton = document.getElementById('close-menu-button');
-const mobileMenu = document.getElementById('mobile-menu');
-const main = document.getElementById('main');
-const mobileMenuOptions = document.getElementById('mobile-menu-list').querySelectorAll('li')
+const mobileMenuOptions = document.getElementById('mobile-menu-list').querySelectorAll('li');
 
 const openMobileMenu = () => {
-  mobileMenu.classList.add('show');
-  header.classList.add('blur');
-  main.classList.add('blur');
+  document.getElementById('mobile-menu').classList.add('show');
+  document.getElementById('header').classList.add('blur');
+  document.getElementById('main').classList.add('blur');
 };
 
 const closeMobileMenu = () => {
-  mobileMenu.classList.remove('show');
-  header.classList.remove('blur');
-  main.classList.remove('blur');
+  document.getElementById('mobile-menu').classList.remove('show');
+  document.getElementById('header').classList.remove('blur');
+  document.getElementById('main').classList.remove('blur');
 };
 
 openMenuButton.addEventListener('click', openMobileMenu);
 closeMenuButton.addEventListener('click', closeMobileMenu);
 
-mobileMenuOptions.forEach(opt => {
-opt.addEventListener('click', closeMobileMenu)
-})
+mobileMenuOptions.forEach((opt) => {
+  opt.addEventListener('click', closeMobileMenu);
+});
