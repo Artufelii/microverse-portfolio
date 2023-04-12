c3cd45bb326109bdeada47fe1588772a93588c8b
Artufelii <artufeii@gmail.com>
2023-04-11 21:44:19 -0600

    a

--- js/modal.js
+++ js/modal.js
@@ -1,13 +1,29 @@
-const buttons = document.getElementById('works').querySelectorAll('button')
+import { card, modal } from './components.js';
+import data from './data.js';
 
-const openModal = () => {
-  document.getElementById('modal').classList.add('open')
-}
+const works = document.getElementById('works');
 
-buttons.forEach((button) => {
-  button.addEventListener('click', openModal)
-})
+data.forEach((work, index) => {
+  const i = index + 1;
+  works.innerHTML += card(work, i);
+  works.innerHTML += modal(work, index);
+});
 
-const closeModal = () => {
-  document.getElementById('modal').classList.remove('open')
-}
+const openModal = (index) => {
+  document.getElementById(`modal-${index}`).classList.add('open');
+};
+
+const closeModal = (index) => {
+  document.getElementById(`modal-${index}`).classList.remove('open');
+};
+
+const openModalButtons = works.querySelectorAll('[data-button="open"]');
+const closeModalButtons = works.querySelectorAll('[data-button="close"]');
+
+closeModalButtons.forEach((button, index) => {
+  button.addEventListener('click', () => closeModal(index));
+});
+
+openModalButtons.forEach((button, index) => {
+  button.addEventListener('click', () => openModal(index));
+});
